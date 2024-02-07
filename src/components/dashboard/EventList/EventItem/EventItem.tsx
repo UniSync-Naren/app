0c82602bf2d927a23ce0b247bab74a8ca120e5c4
Naren <narensreekanth@gmail.com>
2024-02-07 12:29:46 +0800

    [feature-12] Add calendar icon in dashboard

--- src/components/dashboard/EventList/EventItem/EventItem.tsx
+++ src/components/dashboard/EventList/EventItem/EventItem.tsx
@@ -1,5 +1,6 @@
 import styles from './EventItem.module.css';
 import { EventItemProps } from '../../../interface';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 
 export default function EventItem(props: EventItemProps) {
   const startTime = new Date(props.startTime); // Assuming it's the start time, you can adjust accordingly
@@ -14,13 +15,18 @@ export default function EventItem(props: EventItemProps) {
 
   return (
     <div className={styles.eventitem}>
-      <div className={styles.eventname}>
-        <span>{props.eventType}</span>
-        <span>{props.courseid}</span>
+      <div className={styles.icon}>
+        <CalendarTodayOutlinedIcon style={{ color: '#333' }}/>
       </div>
-      <div className={styles.eventtime}>
-        {`${formatTime(startTime)} - ${formatTime(endTime)}`}
-        {props.graded && <span className={styles.grade}>Grade: {props.graded}%</span>}
+      <div className={styles.eventContent}>
+        <div className={styles.eventname}>
+          <span>{props.eventType}</span>
+          <span>{props.courseid}</span>
+        </div>
+        <div className={styles.eventtime}>
+          {`${formatTime(startTime)} - ${formatTime(endTime)}`}
+          {props.graded && <span className={styles.grade}>Grade: {props.graded}%</span>}
+        </div>
       </div>
     </div>
   );
